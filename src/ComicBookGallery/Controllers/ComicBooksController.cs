f0a2e8394f9d74761f76b3cd23bb8998da1ba719
Ali Susanto <ali@digital-safety.sg>
2016-08-14 14:24:54 +0800

    Added comic book repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,8 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+
+
         public ActionResult Detail()
         {
             var comicbook = new ComicBook()
